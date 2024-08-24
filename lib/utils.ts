6488e185bf586813fdc8dcfe80b8456dbe6ffcae
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-08-24 18:07:23 +0530

    Updated individual category get route to include billboard

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
-import { intlFormat } from "date-fns";
 import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
